2f67ccebf98ef8f733313ae5cef932c731f13027
Luis Roca <luis.roca@ge.com>
2019-09-13 07:09:54 -0400

    Avoiding issues when schemaId is undefined

--- src/lib/schema-fetch.js
+++ src/lib/schema-fetch.js
@@ -1,5 +1,8 @@
 const fetchSchema = (schemaRegistry, schemaId) => {
     return new Promise((resolve, reject) => {
+        if (!schemaId) {
+            return reject(new Error(`SchemaId is undefined`));
+        }
         const {protocol, hostname, port, path, auth} = schemaRegistry;
         const requestOptions = {
             host: hostname,
@@ -19,11 +22,13 @@ const fetchSchema = (schemaRegistry, schemaId) => {
             res.on('end', () => {
                 const response = JSON.parse(data);
                 if (res.statusCode !== 200) {
+                    const schemaStr = JSON.parse(data).schema;
+                    const schema = JSON.parse(schemaStr);
+                    resolve(schema);
+                } else {
                     reject(new Error(`Schema registry error: ${response.error_code} - ${response.message}`));
                 }
-                const schemaStr = JSON.parse(data).schema;
-                const schema = JSON.parse(schemaStr);
-                resolve(schema);
+                
             });
         });
 
